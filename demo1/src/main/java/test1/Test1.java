2cc0e477a3f092a6dad875d4a1bf904a543de156
方兴雄 <fxx@czxmsoft.com>
2026-08-28 15:03:21 +0800

    提交提三次测试文件

--- demo1/src/main/java/test1/Test1.java
+++ demo1/src/main/java/test1/Test1.java
@@ -9,4 +9,8 @@ public class Test1 {
     public static void  Test1(){
         System.out.println("Hello World2");
     }
+
+    public static void Test2(){
+        System.out.println("Hello World3");
+    }
 }
